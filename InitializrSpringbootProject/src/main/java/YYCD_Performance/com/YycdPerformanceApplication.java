047b8cb22f8942072452121854d9ef94233a66c4
Chara <164945066+CharaDr39@users.noreply.github.com>
2025-03-11 22:50:00 -0600

    Creacion

---
+++ InitializrSpringbootProject/src/main/java/YYCD_Performance/com/YycdPerformanceApplication.java
@@ -0,0 +1,13 @@
+package YYCD_Performance.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YycdPerformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YycdPerformanceApplication.class, args);
+	}
+
+}
---
+++ InitializrSpringbootProject/src/test/java/YYCD_Performance/com/YycdPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package YYCD_Performance.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YycdPerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
